b7e73fbbd3d77ddfc468086d5e898137f09ce287
JJChan96 <jucham96@gmail.com>
2022-03-21 16:58:32 +0900

    final DB

--- back/etc/DB.sql
+++ back/etc/DB.sql
@@ -9,11 +9,11 @@ CREATE TABLE board (
     date TIMESTAMP NOT NULL,
     hit INT NOT NULL DEFAULT 0,
     likes INT NOT NULL DEFAULT 0
-)
+);
 
 CREATE TABLE user (
     userid VARCHAR(30) NOT NULL PRIMARY KEY,
-    userpw  VARCHAR(50) NOT NULL,
+    userpw VARCHAR(50) NOT NULL,
     userimg VARCHAR(40) NOT NULL,
     username VARCHAR(30) NOT NULL,
     nickname VARCHAR(30) NOT NULL,
@@ -24,14 +24,14 @@ CREATE TABLE user (
     email VARCHAR(50) NOT NULL,
     userintro TEXT NULL,
     level INT NOT NULL DEFAULT 1
-)
+);
 
 CREATE TABLE comment (
     cidx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     comment TEXT NOT NULL,
     c_nickname VARCHAR(30) NOT NULL,
     c_date TIMESTAMP NOT NULL
-)
+);
 
 CREATE TABLE notice (
     idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -40,7 +40,7 @@ CREATE TABLE notice (
     content TEXT NULL,
     date TIMESTAMP NOT NULL,
     hit INT NOT NULL DEFAULT 0
-)
+);
 
 CREATE TABLE QnA (
     idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -50,10 +50,11 @@ CREATE TABLE QnA (
     date TIMESTAMP NOT NULL,
     hit INT NOT NULL DEFAULT 0
 
-)
+);
 
 CREATE TABLE reply (
     ridx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     comment TEXT NOT NULL,
     r_nickname VARCHAR(30) NOT NULL,
     r_date TIMESTAMP NOT NULL
+);
