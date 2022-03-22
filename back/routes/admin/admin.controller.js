3c755eb2b783e0945ece7f4fb5e1a1b91bf8617c
bbb4756 <rlarbfl4756@gmail.com>
2022-03-22 09:40:53 +0900

    라우터분리수정

--- back/routes/admin/admin.controller.js
+++ back/routes/admin/admin.controller.js
@@ -7,3 +7,14 @@ exports.userControl = (req,res)=>{
 exports.boardControl = (req,res)=>{
     res.send('게시물 관리 완료')
 }
+
+exports.search = (req,res)=>{
+    res.send('회원이 쓴 게시물 검색 완료')
+}
+exports.boardControl.hidden = (req,res)=>{
+    res.send('회원 게시물 내림')
+}
+
+exports.userControl.modify = (req,res)=>{
+    res.send('관리자 권한 회원 정보 강제 수정')
+}
