20250ef8c961b2b79cbceb71f36a1c7a8c9a61d1
bbb4756 <rlarbfl4756@gmail.com>
2022-03-23 10:29:35 +0900

    보드 컨트롤러 라우터 분리

--- back/routes/board/index.js
+++ back/routes/board/index.js
@@ -4,11 +4,13 @@ const modifyController = require('./board.modify.controller')
 const noticeController = require('./board.notice.controller')
 const QnAController = require('./board.QnA.controller')
 const writeController = require('./board.write.controller')
+const listController = require('./board.list.controller')
 
 router.post('/write',writeController.write)
 router.post('/modify',modifyController.modify)
 router.post('/QnA',QnAController.QnA)
 router.post('/delete',modifyController.delete)
 router.post('/notice',noticeController.notice)
+router.post('/list', listController.list)
 
 module.exports = router
