5fd0ab75d5e417570f1d88b99d435937dc05b7bc
bbb4756 <rlarbfl4756@gmail.com>
2022-03-31 14:30:31 +0900

    DB수정 최종

--- back/etc/DB.sql
+++ back/etc/DB.sql
@@ -1,6 +1,26 @@
 create database team4;
 use team4;
 
+CREATE TABLE user (
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userid VARCHAR(30) NOT NULL,
+    userpw VARCHAR(50) NOT NULL,
+    userimg VARCHAR(200) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    nickname VARCHAR(30) NOT NULL,
+    address VARCHAR(80) NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    phone VARCHAR(30) NULL,
+    mobile VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    userintro TEXT NULL,
+    level INT NOT NULL DEFAULT 1,
+    available VARCHAR(3) NOT NULL DEFAULT 'on',
+    UNIQUE (userid),
+    UNIQUE (nickname),
+    UNIQUE (email)
+);
+
 CREATE TABLE cate1 (
     idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     category VARCHAR(30) NOT NULL,
@@ -20,39 +40,22 @@ update cate1 set userid = replace(userid,' ', '');
 
 create table cate1_like(
     m_idx int,
-    userid varchar(30)
+    userid varchar(30),
+    FOREIGN key (`m_idx`) REFERENCES `cate1` (`idx`)
 );
 
-create table cate1_bridge(
-    midx int not null,
-    hidx int not null
-);
 create table hashtag(
     hidx int primary key auto_increment,
     hashtag_name varchar(30) not null
 );
 
-
-CREATE TABLE user (
-    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userid VARCHAR(30) NOT NULL,
-    userpw VARCHAR(50) NOT NULL,
-    userimg VARCHAR(200) NOT NULL,
-    username VARCHAR(30) NOT NULL,
-    nickname VARCHAR(30) NOT NULL,
-    address VARCHAR(80) NOT NULL,
-    gender VARCHAR(20) NOT NULL,
-    phone VARCHAR(30) NULL,
-    mobile VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    userintro TEXT NULL,
-    level INT NOT NULL DEFAULT 1,
-    available VARCHAR(3) NOT NULL DEFAULT 'on',
-    UNIQUE (userid),
-    UNIQUE (nickname),
-    UNIQUE (email)
-
+create table cate1_bridge(
+    midx int not null,
+    hidx int not null,
+    FOREIGN key (`midx`) REFERENCES `cate1` (`idx`),
+    FOREIGN key (`hidx`) REFERENCES `hashtag` (`hidx`)
 );
+
 
 CREATE TABLE comment (
     idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -62,7 +65,8 @@ CREATE TABLE comment (
     userid VARCHAR(30) NOT NULL,
     nickname VARCHAR(30) NOT NULL,
     date VARCHAR(30) NOT NULL,
-    updateFlag varchar(5) default 'true'
+    updateFlag varchar(5) default 'true',
+    FOREIGN key (`userid`) REFERENCES `user` (`userid`)
 );
 
 create table notice(
@@ -70,7 +74,9 @@ create table notice(
     writer VARCHAR(30) DEFAULT 'admin',
     title VARCHAR(40) NOT NULL,
     content TEXT NOT NULL,
-    date VARCHAR(30) NOT NULL
+    date VARCHAR(30) NOT NULL,
+    FOREIGN key (`writer`) REFERENCES `user` (`userid`)
+
 );
 
 CREATE TABLE QnA (
@@ -80,27 +86,15 @@ CREATE TABLE QnA (
     userid VARCHAR(30) NOT NULL,
     date VARCHAR(30) NOT NULL,
     category VARCHAR(20) DEFAULT 'qna',
-    hit INT NOT NULL DEFAULT 0
+    hit INT NOT NULL DEFAULT 0,
+    FOREIGN key (`userid`) REFERENCES `user` (`userid`)
 );
 
-CREATE TABLE reply (
-    ridx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    comment TEXT NOT NULL,
-    r_nickname VARCHAR(30) NOT NULL,
-    r_date TIMESTAMP NOT NULL
-);
 
 create table lol_user(
 	userid VARCHAR(30) NOT NULL,
     lol_nickname varchar(30) NOT NULL,
-    UNIQUE(lol_nickname)
+    UNIQUE(lol_nickname),
+    FOREIGN key (`userid`) REFERENCES `user` (`userid`)
 );
 
---CREATE TABLE notice (
---    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---    nickname VARCHAR(20) NOT NULL,
---    title VARCHAR(40) NOT NULL,
---    content TEXT NULL,
---    date TIMESTAMP NOT NULL,
---    hit INT NOT NULL DEFAULT 0
---);
