69c589ca5b3c0a27612903d942f8945be90dc83d
bbb4756 <rlarbfl4756@gmail.com>
2022-04-04 04:43:49 +0900

    CMS html, main css 약간

---
+++ front/css/js/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded',init)
+
+function init(){
+
+    //배너 스크롤 내리면 나오게하기.
+    let trans_box = document.querySelector('#Transparency')
+
+    window.addEventListener('scroll', function(){
+        console.log(window.scrollY)
+        let currentscroll = window.scrollY
+
+        if (currentscroll>20){
+            trans_box.classList.add('trans')
+
+        }else{
+            trans_box.classList.remove('trans')
+        }
+    })
+}
